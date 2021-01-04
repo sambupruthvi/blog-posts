a4f226a6b18f449b668e6affa1aad8cbf4f48524
Pruthvi <psambu@martinonesource.com>
2021-01-04 15:20:04 -0600

    Another way to getUsers by calling action inside another action

--- src/Components/UserHeader.js
+++ src/Components/UserHeader.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchUser } from '../actions';
+// import { fetchUser } from '../actions';
 
 
 class UserHeader extends React.Component {
-    componentDidMount(){
-        this.props.fetchUser(this.props.userId);
-    }
+    // componentDidMount(){
+    //     this.props.fetchUser(this.props.userId);
+    // }
 
     render() {
         // const users = this.props.users.find(user => user.id === this.props.userId)
@@ -25,4 +25,4 @@ class UserHeader extends React.Component {
 const mapStateToProps = (state, ownProps) => {
     return {user: state.users.find(user => user.id === ownProps.userId)}
 }
-export default connect(mapStateToProps, {fetchUser})(UserHeader);
+export default connect(mapStateToProps)(UserHeader);
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,16 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 import _ from 'lodash';
 
+
+// when fetchPosts is called it returns a function redux thunk automatically invokes the dispatch function. The outer dispatch function waits till the api request is complete and then dispatches the result.
+export const fetchPostsAndUsers = () => {
+    return async (dispatch, getState) => {
+        await dispatch(fetchPosts());
+        const userIds = _.uniq(_.map(getState().posts, 'userId'));
+        userIds.forEach(id => dispatch(fetchUser(id)));
+    }
+}
+
 export const fetchPosts = () => {
     return async (dispatch) => {
         const response = await jsonPlaceholder.get('/posts');
@@ -12,16 +22,26 @@ export const fetchPosts = () => {
 };
 
 export const fetchUser = (id) => {
-    return (dispatch) => {
-        _fetchUser(id, dispatch);
+    return async (dispatch) => {
+        const response = await jsonPlaceholder.get(`/users/${id}`);
+        dispatch({
+            type: 'FETCH_USER',
+            payload: response.data
+        })
     }
 }
 
-// private function
-const _fetchUser = _.memoize(async (id, dispatch) => {
-    const response = await jsonPlaceholder.get(`/users/${id}`);
-    dispatch({
-        type: 'FETCH_USER',
-        payload: response.data
-    })
-})
+// export const fetchUser = (id) => {
+//     return (dispatch) => {
+//         _fetchUser(id, dispatch);
+//     }
+// }
+
+// // private function
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+//     dispatch({
+//         type: 'FETCH_USER',
+//         payload: response.data
+//     })
+// })
